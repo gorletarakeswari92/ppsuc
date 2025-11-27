c126165fc2894c320690f5ba5a6d678729e462c4
gorletarakeswari92 <gorletarakeswari92@gmail.com>
2025-11-27 16:05:46 +0530

    Add files via upload

---
+++ area of rectangle,triangle,circle,square.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float l,b,h,s,r;
+	printf ("enter the l,b,h,s,r values:");
+	float R,T,S,C;
+	 R=l*b;
+	T=(0.5)*b*h;
+	S=s*s;
+	 C=3.14*r*r;
+	scanf("%f%f%f%f%f",&l,&b,&h,&s,&r);
+     printf("%.2f %.2f %.2f %.2f",R,T,S,C);
+	return 0;
+	
+}
