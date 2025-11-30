aabe3dec4b0cafe978605fc349b125698f1193b2
gorletarakeswari92 <gorletarakeswari92@gmail.com>
2025-11-30 11:22:11 +0530

    Add files via upload

---
+++ diversifying persons height.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float height;
+	scanf("%f",&height);
+	if(height<150)
+	printf("Person is Dwarf");
+	else if(height>150&&height<=165)
+	printf("Person is average haeighted");
+	else if(height>165&&height<=195)
+	printf("Person is taller");
+	else
+	printf("Abnormal height");
+	return 0;
+}
