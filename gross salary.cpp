d5ef8219c3ee5d9751cacd8dc7745b03c1bf78ec
gorletarakeswari92 <gorletarakeswari92@gmail.com>
2025-11-30 16:17:39 +0530

    Add files via upload

---
+++ gross salary.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int b;
+	float hra,da,gs;
+	scanf("%d",&b);
+	if(b<=10000)
+	{
+		hra=b*0.2;
+		da=b*0.8;
+	}
+	else if(b<=20000)
+	{
+		hra=b*0.25;
+		da=b*0.9;
+	}
+	else
+	{
+		hra=b*0.3;
+		da=b*0.95;
+	}
+	gs=b+da+hra;
+	printf("%.2f",gs);
+	return 0;
+}
---
+++ reaching home.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	scanf("%d%d",&x,&y);
+	if(x*5>=y)
+	printf("yes");
+	else
+	printf("no");
+	return 0;
+}
